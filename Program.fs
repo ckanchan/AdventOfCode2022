3ec9670b4e9964843f7f674e3dffb69a146a85ab
Chai Kanchan <cdkanchan@gmail.com>
2022-12-01 20:04:01 +0000

    Simple (non-recursive) implementation

--- Program.fs
+++ Program.fs
@@ -1,2 +1,28 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let testData = IO.File.ReadAllText "./test"
+let inputData = IO.File.ReadAllText "./input"
+
+type Elf = 
+    | Elf of meals: int array
+
+let totalCalories (Elf meals) =
+    Array.sum(meals)
+
+
+let elvesFromInput (input: string) =
+    input.Split("\n\n")
+    |> Array.map (fun x -> x.Split("\n") |> Array.map int)
+    |> Array.map Elf
+    
+let mostCalories  = Array.map totalCalories >> Array.max
+
+let mostCaloriesOfInput =
+    inputData
+    |> elvesFromInput
+    |> mostCalories
+
+printfn "%d" mostCaloriesOfInput
+
+let topThree =
+     Array.map totalCalories >> Array.sortDescending >> Array.take 3
