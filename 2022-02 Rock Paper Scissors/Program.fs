706501a08e7db88bb45a389a91372125e76b731c
Chai Kanchan <cdkanchan@gmail.com>
2022-12-02 19:46:49 +0000

    Added rockpaperscissors logic

--- 2022-02 Rock Paper Scissors/Program.fs
+++ 2022-02 Rock Paper Scissors/Program.fs
@@ -1,2 +1,33 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type RockPaperScissors =
+    | Rock
+    | Paper
+    | Scissors
+
+let score rockPaperScissor =
+    match rockPaperScissor with
+    | Rock -> 1
+    | Paper -> 2
+    | Scissors -> 3
+
+type Outcome = Win | Lose | Draw
+
+let outcomeScore outcome = 
+    match outcome with
+    | Win -> 6
+    | Draw -> 3
+    | Lose -> 0
+
+let outcome opponent player =
+    match (opponent, player) with
+    | (Paper, Rock) -> Lose
+    | (Scissors, Rock) -> Win
+    | (Rock, Scissors) -> Lose
+    | (Paper, Scissors) -> Win
+    | (Scissors, Paper) -> Lose
+    | (Rock, Paper) -> Win
+    | (_, _) -> Draw
+
+let playerScore opponent player =
+    let itemScore = score player
+    let matchScore = outcome opponent player |> outcomeScore
+    itemScore + matchScore
