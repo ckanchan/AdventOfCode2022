b558366b70dad5fde2f53efbbc678be81d7d4b9f
Chai Kanchan <cdkanchan@gmail.com>
2022-12-03 13:11:52 +0000

    Solution for part 1 implemented

--- 2022-02 Rock Paper Scissors/Program.fs
+++ 2022-02 Rock Paper Scissors/Program.fs
@@ -1,4 +1,5 @@
-﻿type RockPaperScissors =
+﻿open System
+type RockPaperScissors =
     | Rock
     | Paper
     | Scissors
@@ -27,7 +28,41 @@ let outcome opponent player =
     | (Rock, Paper) -> Win
     | (_, _) -> Draw
 
+let inline (^) (opponent: RockPaperScissors)(player: RockPaperScissors) =
+    outcome opponent player
+
 let playerScore opponent player =
     let itemScore = score player
-    let matchScore = outcome opponent player |> outcomeScore
+    let matchScore = ( opponent |> outcome <| player ) |> outcomeScore
     itemScore + matchScore
+
+let RockPaperScissorsFromString str =
+    match str with
+        | "A" -> Some Rock 
+        | "B" -> Some Paper
+        | "C" -> Some Scissors
+        | "X" -> Some Rock
+        | "Y" -> Some Paper
+        | "Z" -> Some Scissors
+        | _ -> None
+
+let gameScoreFromLine (str: string) =
+    let unwrapGame (arr: RockPaperScissors option[]) =
+        match (arr[0], arr[1]) with
+        | Some o, Some p -> Some(playerScore o p)
+        | (_, _) -> None
+
+    str.Split(" ") 
+    |> Array.map RockPaperScissorsFromString 
+    |> unwrapGame
+
+let strategyTotal: seq<string> -> int = Seq.choose gameScoreFromLine >> Seq.sum
+
+let testData = IO.File.ReadLines "./test"
+let inputData = IO.File.ReadLines "./input"
+
+
+
+let result = strategyTotal inputData
+
+printfn "%d" result
