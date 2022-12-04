6355811482afe1145e3eaeddd853653458df0803
Chai Kanchan <cdkanchan@gmail.com>
2022-12-04 17:58:38 +0000

    Solution 2022-03 implemented

--- 2022-03 Rucksack Reorganization/Program.fs
+++ 2022-03 Rucksack Reorganization/Program.fs
@@ -1,2 +1,43 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let testData = IO.File.ReadLines "./test"
+let inputData = IO.File.ReadLines "./input"
+
+let strToCharArray (str: string) =
+    str.ToCharArray()
+
+let itemToPriority character = 
+    let charList = {'A'..'Z'} |> Seq.append {'a'..'z'}
+    {1..52}
+    |> Seq.zip charList
+    |> Map.ofSeq
+    |> Map.find character
+
+let getCommon (ruckSack: string) =
+    ruckSack
+    |> strToCharArray
+    |> Array.splitInto 2
+    |> Array.map Set
+    |> Array.reduce Set.intersect
+
+let prioritiesOfCommonItem =
+    getCommon >> Set.map itemToPriority >> Set.toSeq >> Seq.sum
+
+let getCommonInTripleGroup (ruckSack1:string) (ruckSack2: string)(ruckSack3: string) =
+    [|ruckSack1; ruckSack2; ruckSack3|] 
+        |> Array.map strToCharArray
+        |> Array.map Set.ofArray
+        |> Array.reduce Set.intersect
+
+let prioritiesOfCommonGroup ruckSack1 ruckSack2 ruckSack3 =
+    getCommonInTripleGroup ruckSack1 ruckSack2 ruckSack3 
+    |> Set.map itemToPriority 
+    |> Set.toSeq 
+    |> Seq.sum
+
+let sums (input: string seq) =
+    input
+    |> Seq.chunkBySize 3
+    |> Seq.map (fun chunk -> prioritiesOfCommonGroup chunk[0] chunk[1] chunk[2])
+    |> Seq.sum
+
