4e7620a5b55370e079aa1ef09e650c1109f541e5
Chai Kanchan <cdkanchan@gmail.com>
2022-12-07 19:05:59 +0000

    Implemented 2022-05 stage 2 solution

--- 2022-05 Supply Stacks/Program.fs
+++ 2022-05 Supply Stacks/Program.fs
@@ -48,38 +48,52 @@ let generateCurrentStackPositions (lines: string seq) =
         |> Array.ofSeq
     
     matchedLines
-    
-let rearrangeCrates inputData =
-    let initialState = generateCurrentStackPositions inputData
-    let rec moveCrates stackState numberOfCrates fromStack toStack =
-        let mut updatedStackState: string list[] = Array.copy stackState
-        let moveCrateFrom (sourceStack: string list) (targetStack: string list) =
-            let removedCrate ::restOfSource = sourceStack
-            let resultTargetStack = removedCrate::targetStack
-            (restOfSource, resultTargetStack)
-        
-        let sourceStack = stackState[fromStack]
-        let targetStack = stackState[toStack]
+ 
 
-        let (resultSource, resultTarget) = moveCrateFrom sourceStack targetStack
-        Array.set stackState fromStack resultSource
-        Array.set stackState toStack resultTarget
-        
-        let newCount = numberOfCrates - 1
-        
-        match newCount with
-        | 0 -> stackState
-        | _ -> moveCrates stackState newCount fromStack toStack
+let rec moveCrates9000 (stackState: string list[]) numberOfCrates fromStack toStack =
+    let moveCrateFrom (sourceStack: string list) (targetStack: string list) =
+        let removedCrate ::restOfSource = sourceStack
+        let resultTargetStack = removedCrate::targetStack
+        (restOfSource, resultTargetStack)
+    
+    let sourceStack = stackState[fromStack]
+    let targetStack = stackState[toStack]
+
+    let (resultSource, resultTarget) = moveCrateFrom sourceStack targetStack
+    Array.set stackState fromStack resultSource
+    Array.set stackState toStack resultTarget
+    
+    let newCount = numberOfCrates - 1
+    
+    match newCount with
+    | 0 -> stackState
+    | _ -> moveCrates9000 stackState newCount fromStack toStack
+
+let moveCrates9001 (stackState: string list[]) numberOfCrates fromStack toStack =
+    let sourceStack = stackState[fromStack]
+    let targetStack = stackState[toStack]
+    let (cratesToMove, resultSource) = List.splitAt numberOfCrates sourceStack
+    let resultTarget = cratesToMove @ targetStack
+    Array.set stackState fromStack resultSource
+    Array.set stackState toStack resultTarget
+
+    stackState
+
+let rearrangeCrates crateMover inputData =
+    let initialState = generateCurrentStackPositions inputData
     
     let folder stackState input =
         match InstructionMatcher().TryTypedMatch(input) with
         | Some m -> 
             let adjustedSourceStackIdx = m.source.AsInt - 1
             let adjustedTargetStackIdx = m.destination.AsInt - 1
-            moveCrates stackState m.itemCount.AsInt adjustedSourceStackIdx adjustedTargetStackIdx
+            crateMover stackState m.itemCount.AsInt adjustedSourceStackIdx adjustedTargetStackIdx
         |_ -> stackState
     
     Seq.fold folder initialState inputData 
     
-let result = rearrangeCrates inputData |> Seq.choose Seq.tryHead |> String.concat ""
+let result = rearrangeCrates moveCrates9000 inputData |> Seq.choose Seq.tryHead |> String.concat ""
 printfn "%A" result
+
+let result2 = rearrangeCrates moveCrates9001 inputData |> Seq.choose Seq.tryHead |> String.concat ""
+printfn "%A" result2
