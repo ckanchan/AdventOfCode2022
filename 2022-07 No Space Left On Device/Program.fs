6705990a7ff9a7c129a6c4a3010b1c5354460b46
Chai Kanchan <cdkanchan@gmail.com>
2022-12-07 19:40:53 +0000

    Initial commit

---
+++ 2022-07 No Space Left On Device/Program.fs
@@ -0,0 +1,11 @@
+﻿#if INTERACTIVE
+#r "nuget:FSharp.Text.RegexProvider"
+#endif
+
+open System.IO
+open FSharp.Text.RegexProvider
+open FSharp.Text.RegexExtensions
+
+let testData = File.ReadLines "./test"
+let inputData = File.ReadLines "./input"
+
