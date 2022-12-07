c456a251092174b0716aa5c85be3d06d39b8454f
Chai Kanchan <cdkanchan@gmail.com>
2022-12-07 19:29:12 +0000

    2022-06 implemented part 1 and 2 solution

--- 2022-06 Tuning Trouble/Program.fs
+++ 2022-06 Tuning Trouble/Program.fs
@@ -1,2 +1,22 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let testData = File.ReadAllLines "./test"
+let inputData = File.ReadAllLines "./input"
+
+let startOfMarkerIdx (window: int) (s: string) =
+    s.ToCharArray()
+    |> Seq.windowed window
+    |> Seq.takeWhile (fun charWindow -> Array.distinct charWindow |> Array.length <> window)
+    |> Seq.length
+    |> (+) window
+
+let startOfPacketMarkerIdx = startOfMarkerIdx 4
+let startOfMessageMarkerIdx = startOfMarkerIdx 14
+
+inputData
+|> Array.map startOfPacketMarkerIdx
+|> Array.iter (printfn "%d")
+
+inputData
+|> Array.map startOfMessageMarkerIdx
+|> Array.iter(printfn "%d")
