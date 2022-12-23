325a4c3618a1f98f0c014e2f65570164221e6498
Chai Kanchan <cdkanchan@gmail.com>
2022-12-23 19:26:56 +0000

    Implemented 2022-09 solution part two

--- 2022-09 Rope Bridge/Program.fs
+++ 2022-09 Rope Bridge/Program.fs
@@ -2,6 +2,7 @@
 open System.Diagnostics
 
 let testData = File.ReadLines "./test"
+let longTestData = File.ReadLines "./longTest"
 let inputData = File.ReadLines "./input"
 
 let parseInt (str: string) =
@@ -16,11 +17,30 @@ type PointXY =
     static member origin = {x=0;y=0}
 
 [<Struct>][<DebuggerDisplay("{print()t}")>]
-type Rope = 
+type ShortRope = 
     {head: PointXY; tail: PointXY}
     member rope.print = sprintf "||head: %s | tail: %s||" rope.head.print rope.tail.print
 
-let startPosition = {head=PointXY.origin; tail=PointXY.origin}
+type LongRope = PointXY list
+
+type Rope =
+    | Short of ShortRope
+    | Long of PointXY list
+
+    member rope.print =
+        match rope with
+        | Short r -> 
+            sprintf "||head: %s | tail: %s||" r.head.print r.tail.print
+        | Long r ->
+            r |> List.map (fun point -> sprintf "|" + point.print + "|" ) |> String.concat "<>"
+
+let startPosition = Rope.Short {head=PointXY.origin; tail=PointXY.origin}
+let longRopeOrigin length = PointXY.origin |> List.replicate length |> Rope.Long
+
+let ropeTail rope = 
+    match rope with
+    | Short s -> s.tail
+    | Long l -> List.last l
 
 type Direction = Left | Right | Up | Down
 let directionFromString str = 
@@ -84,60 +104,105 @@ let positionDifference point1 point2 =
         | (LessThan, LessThan) -> Diagonal (LowerLeft, horizontalMagnitude, verticalMagnitude)
         | (LessThan, GreaterThan) -> Diagonal (UpperLeft, horizontalMagnitude, verticalMagnitude)
     
+let newTailPosition headPosition tailPosition =
+    match positionDifference headPosition tailPosition with
+    | Overlap -> tailPosition
+    | Left magnitude when magnitude = 1 -> tailPosition
+    | Right magnitude when magnitude = 1 -> tailPosition
+    | Above magnitude when magnitude = 1 -> tailPosition
+    | Below magnitude when magnitude = 1 -> tailPosition
+    | Diagonal (_, horizontalMagnitude, verticalMagnitude) when (horizontalMagnitude, verticalMagnitude) = (1,1) -> tailPosition
+    | Left _ -> {tailPosition with x = tailPosition.x - 1}
+    | Right _ -> {tailPosition with x = tailPosition.x + 1}
+    | Above _ -> {tailPosition with y = tailPosition.y + 1}
+    | Below _ -> {tailPosition with y = tailPosition.y - 1}
+    | Diagonal (diagonalDirection, _, _) ->
+        match diagonalDirection with
+        | UpperLeft -> {tailPosition with x = tailPosition.x - 1; y = tailPosition.y + 1}
+        | UpperRight -> {tailPosition with x = tailPosition.x + 1; y = tailPosition.y + 1}
+        | LowerLeft -> {tailPosition with x = tailPosition.x - 1; y = tailPosition.y - 1}
+        | LowerRight -> {tailPosition with x = tailPosition.x + 1; y = tailPosition.y - 1}
 
+let newLongRopePosition (longRope: LongRope)(moveBy: Move) =
+    let shiftLongRopeByOne longRope =
+        longRope 
+        |> List.fold (
+            fun (acc: LongRope) (elem:PointXY) ->
+                match acc with
+                | [] -> 
+                    transform moveBy.direction 1 elem |> List.singleton
+                | head::_ ->
+                    let newElemPosition = newTailPosition head elem
+                    newElemPosition::acc
+        ) []
+        |> List.rev
 
+    [|1..moveBy.magnitude|]
+    |> Array.scan (
+        fun acc elem -> shiftLongRopeByOne acc
+    ) longRope
 
-let moveRope (by: Move) (rope: Rope) =
-    let newTailPosition tailPosition headPosition =
-        match positionDifference headPosition tailPosition with
-        | Overlap -> tailPosition
-        | Left magnitude when magnitude = 1 -> tailPosition
-        | Right magnitude when magnitude = 1 -> tailPosition
-        | Above magnitude when magnitude = 1 -> tailPosition
-        | Below magnitude when magnitude = 1 -> tailPosition
-        | Diagonal (_, horizontalMagnitude, verticalMagnitude) when (horizontalMagnitude, verticalMagnitude) = (1,1) -> tailPosition
-        | Left _ -> {tailPosition with x = tailPosition.x - 1}
-        | Right _ -> {tailPosition with x = tailPosition.x + 1}
-        | Above _ -> {tailPosition with y = tailPosition.y + 1}
-        | Below _ -> {tailPosition with y = tailPosition.y - 1}
-        | Diagonal (diagonalDirection, _, _) ->
-            match diagonalDirection with
-            | UpperLeft -> {tailPosition with x = tailPosition.x - 1; y = tailPosition.y + 1}
-            | UpperRight -> {tailPosition with x = tailPosition.x + 1; y = tailPosition.y + 1}
-            | LowerLeft -> {tailPosition with x = tailPosition.x - 1; y = tailPosition.y - 1}
-            | LowerRight -> {tailPosition with x = tailPosition.x + 1; y = tailPosition.y - 1}      
+let newRopePositions rope moveBy =
+    [|1..moveBy.magnitude|]
+    |> Array.scan (
+        fun acc elem ->
+            let newHead = transform moveBy.direction 1 acc.head
+            let newTail = newTailPosition newHead acc.tail
+            {head=newHead; tail=newTail}
+    ) rope
 
-    let newRopePositions rope =
-        [|1..by.magnitude|]
-        |> Array.scan (
-            fun acc elem ->
-                let newHead = transform by.direction 1 acc.head
-                let newTail = newTailPosition acc.tail newHead
-                {head=newHead; tail=newTail}
-        ) rope
-            
-    newRopePositions rope
+let moveRope (moveBy: Move) (rope: Rope) =        
+    let shortRopeAccumulator (acc: ShortRope)(elem: int) = 
+        let newHead = transform moveBy.direction 1 acc.head
+        let newTail = newTailPosition newHead acc.tail
+        {head=newHead; tail=newTail}
+        
+    let longRopeAccumulator (acc: LongRope)(elem: int) = 
+        acc 
+        |> List.fold (
+            fun (acc: LongRope) (elem:PointXY) ->
+                match acc with
+                | [] -> 
+                    transform moveBy.direction 1 elem |> List.singleton
+                | head::_ ->
+                    let newElemPosition = newTailPosition head elem
+                    newElemPosition::acc
+        ) []
+        |> List.rev
+    
+    
+    let iterArray = [|1..moveBy.magnitude|]
+
+    match rope with
+        | Short r ->
+            iterArray |> Array.scan shortRopeAccumulator r |> Array.map Rope.Short
+        | Long r ->
+            iterArray |> Array.scan longRopeAccumulator r |> Array.map Rope.Long
 
     
-let ropesFromStringSeq (s: string seq) =
+let ropesFromStringSeq (origin: Rope) (s: string seq) =
+    let origin = Array.singleton origin
     s
     |> Seq.choose parseLine
     |> Seq.fold (fun acc elem ->
             let ropeEndPosition = Array.last acc
             let newPositions = moveRope elem ropeEndPosition
             Array.append acc newPositions
-    ) [|startPosition|]
+    ) origin
 
-let test =  ropesFromStringSeq testData
-let input = ropesFromStringSeq inputData
-
+let test =  ropesFromStringSeq startPosition testData
+let test2 = ropesFromStringSeq (longRopeOrigin 2) testData
+let input = ropesFromStringSeq startPosition inputData
 
 let printRopes (ropeArray: Rope array) =
     ropeArray 
     |> Array.map (fun r -> r.print)
     |> Array.iter (printfn "%s")
 
-let countDistinctTailPositions ropes = 
-    ropes |> Array.map (fun r -> r.tail) |> Array.distinct |> Array.length
+let countDistinctTailPositions ropes =          
+    ropes |> Array.map ropeTail |> Array.distinct |> Array.length
 
-
+inputData
+|> ropesFromStringSeq (longRopeOrigin 10)
+|> countDistinctTailPositions
+|> printfn "%d"
