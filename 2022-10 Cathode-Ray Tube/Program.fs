0d54c833f46265df0733e9917bcf1ef9323e2544
Chai Kanchan <cdkanchan@gmail.com>
2022-12-24 14:52:14 +0000

    2022-10 initial commit

---
+++ 2022-10 Cathode-Ray Tube/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+
+let testData = File.ReadLines("./test")
+let inputData = File.ReadLines("./input")
+
+let tryParseInt (str: string) =
+    match System.Int32.TryParse(str) with
+    | (true, number) -> Some number
+    | (_, _) -> None
+
+type CPUInstruction =
+    | NoOp
+    | AddX of int
+
+let cpuInstructionFromString (str: string) =
+    let elements = str.Split(" ")
+    match elements[0] with
+    | "noop" -> 
+        Some NoOp
+    | "addx" ->
+        match tryParseInt(elements[1]) with
+        | Some number -> AddX number |> Some
+        | None -> None
+    | _ -> 
+        None
