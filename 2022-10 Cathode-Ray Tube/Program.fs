23f7b6300d186f22101ba105c189c0231748e000
Chai Kanchan <cdkanchan@gmail.com>
2022-12-24 20:44:37 +0000

    2022-10 part 1 solution

--- 2022-10 Cathode-Ray Tube/Program.fs
+++ 2022-10 Cathode-Ray Tube/Program.fs
@@ -11,6 +11,12 @@ let tryParseInt (str: string) =
 type CPUInstruction =
     | NoOp
     | AddX of int
+    static member shortTest = [NoOp; AddX 3; AddX -5] |> Seq.ofList
+
+type CPUState = 
+    {registerX: int; cycle: int}
+    static member initial = {registerX=1;cycle=1}
+    member cpu.signalStrength = cpu.registerX * cpu.cycle
 
 let cpuInstructionFromString (str: string) =
     let elements = str.Split(" ")
@@ -23,3 +29,41 @@ let cpuInstructionFromString (str: string) =
         | None -> None
     | _ -> 
         None
+
+let processInstruction (cpuState: CPUState) instruction =
+    match instruction with
+    | NoOp ->
+         Array.singleton {cpuState with cycle = cpuState.cycle + 1}
+    | AddX value ->
+        [|
+            {cpuState with cycle = (cpuState.cycle + 1)};
+            {registerX=cpuState.registerX + value; cycle = cpuState.cycle + 2}
+        |]
+
+let processInstructionL (cpuStates: CPUState array) instruction =
+    let state = Array.last cpuStates 
+    processInstruction state instruction
+
+let signalStrength cpuState =
+    let currentCycle = cpuState.cycle - 1
+    currentCycle * cpuState.registerX
+
+let cpuStatesFromInstructions (instructions: CPUInstruction seq) =
+    instructions
+    |> Seq.scan processInstructionL [|CPUState.initial|]
+    |> Array.concat
+
+let getInterestingSignals (cpuStates: CPUState array) =
+    let decrement i = i - 1
+    let interestingSignalIndices = [|20..40..220|] |> Array.map decrement
+    interestingSignalIndices 
+    |> Array.choose (fun i -> Array.tryItem  i cpuStates)
+    |> Array.map (fun s -> {|x=s.registerX;cycle=s.cycle; signalStrength = s.signalStrength|})
+
+
+let test = testData |> Seq.choose cpuInstructionFromString |> cpuStatesFromInstructions
+
+test
+|> getInterestingSignals
+|> Array.sumBy (fun x -> x.signalStrength)
+|> printfn "%d"
